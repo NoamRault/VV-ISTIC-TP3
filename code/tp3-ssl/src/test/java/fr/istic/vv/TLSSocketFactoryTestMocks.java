bc3ad091f7d44f0eb38dcffc4692f8851b812a4a
Gallais Florian <florian.gallais@etudiant.univ-rennes1.fr>
2024-12-06 12:24:50 +0100

    Exercice Mock

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -1,6 +1,7 @@
 package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,8 +9,77 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TLSSocketFactoryTestMocks {
 
+    @Test
+    public void testPrepareSocket_withSupportedAndEnabledProtocols() {
+        // Création du mock de SSLSocket
+        SSLSocket mockSocket = mock(SSLSocket.class);
+
+        // Définition des protocoles pris en charge et activés
+        when(mockSocket.getSupportedProtocols()).thenReturn(new String[] { "TLSv1.2", "TLSv1.1", "TLSv1" });
+        when(mockSocket.getEnabledProtocols()).thenReturn(new String[] { "TLSv1" });
+
+        // Création de l'instance de TLSSocketFactory et préparation du socket
+        TLSSocketFactory factory = new TLSSocketFactory();
+        factory.prepareSocket(mockSocket);
+
+        // Vérification que les protocoles activés ont été définis correctement
+        verify(mockSocket).setEnabledProtocols(new String[] { "TLSv1.2", "TLSv1.1", "TLSv1" });
+    }
+
+    @Test
+    public void testPrepareSocket_withNoEnabledProtocols() {
+        // Création du mock de SSLSocket
+        SSLSocket mockSocket = mock(SSLSocket.class);
+
+        // Définition des protocoles pris en charge et activés
+        when(mockSocket.getSupportedProtocols()).thenReturn(new String[] { "TLSv1.2", "TLSv1.1", "TLSv1" });
+        when(mockSocket.getEnabledProtocols()).thenReturn(new String[] {});
+
+        // Création de l'instance de TLSSocketFactory et préparation du socket
+        TLSSocketFactory factory = new TLSSocketFactory();
+        factory.prepareSocket(mockSocket);
+
+        // Vérification que les protocoles activés ont été définis correctement
+        verify(mockSocket).setEnabledProtocols(new String[] { "TLSv1.2", "TLSv1.1", "TLSv1" });
+    }
+
+    @Test
+    public void testPrepareSocket_withNoSupportedProtocols() {
+        // Création du mock de SSLSocket
+        SSLSocket mockSocket = mock(SSLSocket.class);
+
+        // Définition des protocoles pris en charge et activés
+        when(mockSocket.getSupportedProtocols()).thenReturn(new String[] {});
+        when(mockSocket.getEnabledProtocols()).thenReturn(new String[] {"TLSv1.1", "TLSv1" });
+
+        // Création de l'instance de TLSSocketFactory et préparation du socket
+        TLSSocketFactory factory = new TLSSocketFactory();
+        factory.prepareSocket(mockSocket);
+
+        // Vérification que les protocoles activés ont été définis correctement
+        verify(mockSocket).setEnabledProtocols(new String[] { "TLSv1.1", "TLSv1" });
+    }
+
+
+    @Test
+    public void testPrepareSocket_withNothing() {
+        // Création du mock de SSLSocket
+        SSLSocket mockSocket = mock(SSLSocket.class);
+
+        // Définition des protocoles pris en charge et activés comme vides
+        when(mockSocket.getSupportedProtocols()).thenReturn(new String[] {});
+        when(mockSocket.getEnabledProtocols()).thenReturn(new String[] {});
+
+        // Création de l'instance de TLSSocketFactory et préparation du socket
+        TLSSocketFactory factory = new TLSSocketFactory();
+        factory.prepareSocket(mockSocket);
+
+        // Vérification que setEnabledProtocols n'a pas été appelé
+        verify(mockSocket, never()).setEnabledProtocols(any(String[].class));
+    }
 
 }
